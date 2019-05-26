da589f3eced9ce68135f792c127084a3b729b152
891760188 <891760198@qq.com>
2019-05-26 21:56:32 +0800

    addcode

--- src/main/java/com/zpc/item/runner/ItemApp.java
+++ src/main/java/com/zpc/item/runner/ItemApp.java
@@ -2,6 +2,7 @@ package com.zpc.item.runner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 
 @SpringBootApplication//申明这是一个Spring Boot项目22
+@EnableEurekaClient//eureka客户端
 @ComponentScan(basePackages = {"com.zpc.item.controller","com.zpc.item.service"})//手动指定bean组件扫描范围
 public class ItemApp {
     public static void main(String[] args) {
